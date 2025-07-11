327bf3c10d6ddb8f53c3e88cd1c2ac358d7545b8
olaguna7 <oscarlagunacaminero@gmail.com>
2025-07-11 19:17:43 +0200

    Add user entity

---
+++ src/main/java/com/oscar/usermicroservice/entity/User.java
@@ -0,0 +1,18 @@
+package com.oscar.usermicroservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String username;
+    private String email;
+    private String password;
+}
