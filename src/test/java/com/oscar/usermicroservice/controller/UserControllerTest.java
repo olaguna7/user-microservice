db91ffd4a971400244e9b33b456df20dc3196efd
olaguna7 <oscarlagunacaminero@gmail.com>
2025-07-12 15:00:56 +0200

    Add tests for user service and user controller

---
+++ src/test/java/com/oscar/usermicroservice/controller/UserControllerTest.java
@@ -0,0 +1,88 @@
+package com.oscar.usermicroservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oscar.usermicroservice.dto.UserCreateDTO;
+import com.oscar.usermicroservice.dto.UserDTO;
+import com.oscar.usermicroservice.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Test
+    void shouldReturnAllUsers_whenGetAll() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnUser_whenGetById() throws Exception {
+        UserDTO dto = new UserDTO("user", "mail@mail.com");
+        when(userService.findById(1L)).thenReturn(dto);
+
+        mockMvc.perform(get("/users/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("user"))
+                .andExpect(jsonPath("$.email").value("mail@mail.com"));
+    }
+
+    @Test
+    void shouldReturnNotFound_whenGetByIdNotFound() throws Exception {
+        when(userService.findById(200L)).thenThrow(new EntityNotFoundException());
+        
+        mockMvc.perform(get("/users/200"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldReturnBadRequest_whenCreateUserWithInvalidEmail() throws Exception {
+        UserCreateDTO dto = new UserCreateDTO("user", "user@user", "123");
+        when(userService.createUser(any(UserCreateDTO.class)))
+                .thenThrow(new DataIntegrityViolationException("Error in email format"));
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldReturnBadRequest_whenCreateUserWithNullField() throws Exception {
+        // DTO with null username
+        UserCreateDTO dto = new UserCreateDTO();
+        dto.setEmail("hola@hola.com");
+        dto.setPassword("12345");
+
+        when(userService.createUser(any(UserCreateDTO.class)))
+                .thenThrow(new DataIntegrityViolationException("Null field cannot be inserted"));
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isBadRequest());
+    }
+
+}
---
+++ src/test/java/com/oscar/usermicroservice/service/UserServiceTest.java
@@ -0,0 +1,88 @@
+package com.oscar.usermicroservice.service;
+
+import com.oscar.usermicroservice.dto.UserCreateDTO;
+import com.oscar.usermicroservice.dto.UserDTO;
+import com.oscar.usermicroservice.entity.User;
+import com.oscar.usermicroservice.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+class UserServiceTest {
+
+    @Test
+    @DisplayName("findById should return the UserDTO when user exists")
+    void shouldReturnUserDTO_whenUserExists() {
+        UserRepository mockRepository = mock(UserRepository.class);
+        UserService userService = new UserService(mockRepository);
+
+        User user = new User(1L, "User1", "user@user.com", "12345");
+
+        when(mockRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        UserDTO userDTO = userService.findById(1L);
+        assertThat(userDTO).isEqualTo(new UserDTO("User1", "user@user.com"));
+
+        verify(mockRepository).findById(1L);
+    }
+
+    @Test
+    @DisplayName("findById should throw an EntityNotFoundException when user does not exist")
+    void shouldThrowUserNotFoundException_whenUserDoesNotExist() {
+        UserRepository mockRepository = mock(UserRepository.class);
+        UserService userService = new UserService(mockRepository);
+
+        when(mockRepository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> userService.findById(1L))
+                .isInstanceOf(EntityNotFoundException.class);
+
+        verify(mockRepository).findById(1L);
+    }
+
+    @Test
+    void shouldCreateUser_whenDataIsValid() {
+        UserRepository mockRepository = mock(UserRepository.class);
+        UserService userService = new UserService(mockRepository);
+
+        UserCreateDTO userToCreate = new UserCreateDTO("user2", "user2@user.com", "password2");
+
+        User userEntity = new User(1L, "user2", "user2@user.com", "password2");
+        when(mockRepository.save(any(User.class))).thenReturn(userEntity);
+
+        UserCreateDTO result = userService.createUser(userToCreate);
+
+        assertThat(result).isEqualTo(userToCreate); // O compara campo a campo si no tienes equals en el DTO
+    }
+
+    @Test
+    void shouldDeleteUser_whenUserExists() {
+        UserRepository mockRepository = mock(UserRepository.class);
+        UserService userService = new UserService(mockRepository);
+
+        User user = new User(1L, "user1", "user1@user.com", "password1");
+
+        userService.deleteUser(1L);
+        verify(mockRepository).deleteById(1L);
+    }
+
+    @Test
+    void shouldThrowEmptyResultDataAccessException_whenUserDoesNotExist() {
+        UserRepository mockRepository = mock(UserRepository.class);
+        UserService userService = new UserService(mockRepository);
+
+        doThrow(new EmptyResultDataAccessException(1)).when(mockRepository).deleteById(1L);
+
+        assertThatThrownBy(() -> userService.deleteUser(1L))
+                .isInstanceOf(EmptyResultDataAccessException.class);
+
+        verify(mockRepository).deleteById(1L);
+    }
+}
