4759b9c69003accc337ae69c6879be3aff9b9639
olaguna7 <oscarlagunacaminero@gmail.com>
2025-07-12 17:32:33 +0200

    implemented toDTOList method for roles

---
+++ src/main/java/com/oscar/usermicroservice/mapper/RoleMapper.java
@@ -0,0 +1,25 @@
+package com.oscar.usermicroservice.mapper;
+
+import com.oscar.usermicroservice.dto.RoleDTO;
+import com.oscar.usermicroservice.entity.Role;
+
+import java.util.List;
+
+public class RoleMapper {
+
+    private RoleMapper() {}
+
+    public static RoleDTO toDTO(Role role) {
+        return new RoleDTO(role.getName());
+    }
+
+    public static List<RoleDTO> toDTOList(List<Role> roles) {
+        return roles.stream().map(RoleMapper::toDTO).toList();
+    }
+
+    public static Role toEntity(RoleDTO roleDTO) {
+        Role role = new Role();
+        role.setName(roleDTO.getName());
+        return role;
+    }
+}
